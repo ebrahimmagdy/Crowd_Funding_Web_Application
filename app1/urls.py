8644fcf520305a8b2c5cbc50cc5b773804e1857e
Ahmedsamymahrous <asamy0037@gmail.com>
2021-03-30 00:23:25 +0200

    linking the login and signup pages to the home page

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from app1 import views
 
 urlpatterns = [
-    path("home", views.home)
+    path("home", views.home, name="home"),
+    path("login", views.login, name="login"),
+    path("signup", views.signup, name="signup"),
 ]
