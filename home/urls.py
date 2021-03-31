137a755503c9c6620fd71aabeebacf887149ecc2
Ahmedsamymahrous <asamy0037@gmail.com>
2021-03-31 22:17:56 +0200

    the layout added  to our skelton

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path("home", views.home, name="home"),
+    path("login", views.login, name="login"),
+    path("signup", views.signup, name="signup"),
+    path("project", views.project, name="project"),
+]
