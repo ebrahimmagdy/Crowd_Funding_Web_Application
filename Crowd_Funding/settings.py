1a37b037e382f14b1349937eb966917d594418bd
Ahmedsamymahrous <asamy0037@gmail.com>
2021-04-03 09:26:58 +0200

    signup, login and logout

--- Crowd_Funding/settings.py
+++ Crowd_Funding/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'users',
+    'crispy_forms',
+
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -70,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Crowd_Funding.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -83,8 +85,6 @@ DATABASES = {
         'HOST': '127.0.0.1',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -104,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -118,12 +117,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'     # Override the default (default = accounts/profile)
+LOGIN_URL = 'login'
+
--- home/urls.py
+++ home/urls.py
@@ -3,10 +3,10 @@ from .views import home, user, project, profile
 
 urlpatterns = [
     path("home", home.home, name="home"),
-    path("login", user.login, name="login"),
+    # path("login", user.login, name="login"),
     path("signup", user.signup, name="signup"),
     path("project", project.project, name="project"),
     path("activate", user.activate, name="activate"),
-    path("profile", profile.profile, name="profile"),
+    # path("profile", profile.profile, name="profile"),
 
 ]
